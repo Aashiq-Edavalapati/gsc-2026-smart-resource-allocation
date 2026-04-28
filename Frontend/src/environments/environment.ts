7587ac731feabcd31d4967abd7f16d7fa644489b
Aashiq-Edavalapati <aashiqedavalapati58@gmail.com>
2026-04-28 23:45:21 +0530

    fix deployment

---
+++ Frontend/src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCzh9349ev3eoGsaAEU0rmbj6xQk6sEEec',
+    authDomain: 'gsc-2026-fea7c.firebaseapp.com',
+    projectId: 'gsc-2026-fea7c',
+    storageBucket: 'gsc-2026-fea7c.firebasestorage.app',
+    messagingSenderId: '118981447809',
+    appId: '1:118981447809:web:your_web_app_id'
+  },
+  // apiUrl: 'https://backend-349232024775.asia-south1.run.app/api/v1',
+  apiUrl: 'http://localhost:8080/api/v1',
+  api: {
+    url: 'http://localhost:8080/api/v1'
+  }
+};
